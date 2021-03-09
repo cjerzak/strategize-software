8030f4b508ba053a16e427740c2d8e768653f42f
cjerzak <connor.jerzak@gmail.com>
2021-03-09 15:07:36 -0500

    package skeleton

---
+++ optimalcausalities/R/readme2_helper_fxns.R
@@ -0,0 +1,187 @@
+#' cleanme
+#' 
+#' Standard preprocessing code for ASCII texts. Removes HTML tags, URLs, linebreaks. Converts standard emoticons to tokens. Removes non-informative punctuation.
+#' 
+#' @param my_text Vector of character strings containing the raw document texts.
+#' 
+#' @param finalEncoding A character string indicating the desired encoding for the text vector (default = "ASCII")
+#' 
+#' @return A vector of character strings with the processed texts, each token is separated by a space.
+#' 
+#' @export 
+#' 
+#' 
+cleanme <- function(my_text,finalEncoding = "ASCII"){ 
+  ### Convert to ASCII encoding
+  my_text <- sapply(my_text, 
+                    function(x){ startingEncoding <- Encoding(x)
+                      if(startingEncoding == "unknown"){ x<-iconv(x, from = finalEncoding, to = finalEncoding, sub = '', mark = T) } 
+                      if(startingEncoding != "unknown"){ x<-iconv(x, from = startingEncoding, to = finalEncoding, sub = '', mark = T) }
+                      return(x) })
+  names(my_text) <- NULL 
+  
+  ## Remove HTML/XML tags
+  my_text <- gsub(my_text, pattern = "<.*?>", replace = " <html> ") 
+  
+  ## Remove newline, carriage return and tabs
+  my_text <- gsub(my_text, pattern = "\n", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\r", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\t", replace = " ") 
+  
+  ## Convert URLs to generic <url> feature
+  url_pattern1 <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+  url_pattern2 <- " ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)"
+  my_text <- gsub(my_text, pattern = url_pattern1, replace = " <url> ") 
+  my_text <- gsub(my_text, pattern = url_pattern2, replace = " <url> ") 
+  
+  ## Convert standard emoticons
+  my_text <- gsub(my_text, pattern = ":-\\)", replace = " <smile> ")
+  my_text <- gsub(my_text, pattern = "\\^_\\^", replace = " <smile> ")
+  my_text <- gsub(my_text, pattern = ":\\)", replace = " <smile> ")
+  my_text <- gsub(my_text, pattern = "\\(:", replace = " <smile> ")
+  my_text <- gsub(my_text, pattern = "\\(-:", replace = " <smile> ")
+  my_text <- gsub(my_text, pattern = ":D", replace = " <smile> ")
+  my_text <- gsub(my_text, pattern = ":-\\(", replace = " <sadface> ")
+  my_text <- gsub(my_text, pattern = ":\\(", replace = " <sadface> ")
+  my_text <- gsub(my_text, pattern = "\\):", replace = " <sadface> ")
+  my_text <- gsub(my_text, pattern = "\\)-:", replace = " <sadface> ")
+  my_text <- gsub(my_text, pattern = "D:", replace = " <sadface> ")
+  
+  ## Remove punctuation
+  my_text <- gsub(my_text, pattern = "\\s*-\\B|\\B-\\s*", replace = " ") 
+  my_text <- gsub(my_text, pattern = " \\- ", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\--", replace = " ") 
+  my_text <- gsub(my_text, pattern = "@\\S+", replace = " <user> ") ## Twitter-specific, any @text converted to a generic <user> feature
+  
+  my_text <- gsub(my_text, pattern = "[[:digit:]]+", replace = " <number> ") ## Convert numbers to generic <number> feature
+  
+  ## More removal of punctuation
+  my_text <- gsub(my_text, pattern = '\\*', replace = " ")
+  my_text <- gsub(my_text, pattern = "\\.\\.\\.", replace = " … ")
+  my_text <- gsub(my_text, pattern = "\\.", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\:", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\;", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\,", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\(", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\)", replace = " ") 
+  my_text <- gsub(my_text, pattern = '\\]', replace = " ")
+  my_text <- gsub(my_text, pattern = '\\[', replace = " ")
+  my_text <- gsub(my_text, pattern = "\\/", replace = " ") 
+  my_text <- gsub(my_text, pattern = "\\\\", replace = " ") 
+  my_text <- gsub(my_text, pattern = '\\"', replace = " ") 
+  my_text <- gsub(my_text, pattern = '\\¨', replace = " ")
+  my_text <- gsub(my_text, pattern = "\\ô", replace = " ") 
+  
+  ## Informative punctuation become their own features
+  my_text <- gsub(my_text, pattern = '\\=', replace = " = ")
+  my_text <- gsub(my_text, pattern = '\\+', replace = " + ")
+  my_text <- gsub(my_text, pattern = "\\!", replace = " ! ") 
+  my_text <- gsub(my_text, pattern = "\\?", replace = " ? ") 
+  my_text <- gsub(my_text, pattern = "\\õ", replace = " ' ") 
+  my_text <- gsub(my_text, pattern = "\\õ", replace = " ' ")
+  
+  ## Contractions are split out
+  my_text <- gsub(my_text, pattern = "\\'s", replace = " \\'s ")
+  my_text <- gsub(my_text, pattern = "n\\'t ", replace = " n\\'t ")
+  my_text <- gsub(my_text, pattern = "\\'d ", replace = " \\'d ") 
+  my_text <- gsub(my_text, pattern = "\\'re ", replace = " \\'re ") 
+  my_text <- gsub(my_text, pattern = "\\'ve ", replace = " \\'ve ") 
+  my_text <- gsub(my_text, pattern = "\\'ll ", replace = " \\'ll ") 
+  my_text <- gsub(my_text, pattern = "\\'m ", replace = " \\'m ") 
+  
+  ## Standard abbreviations
+  my_text <- gsub(my_text, pattern = "gov\\'t", replace = " government ") 
+  
+  ### Non-separating punctuation
+  my_text <- gsub(my_text, pattern = "%", replace = "") 
+  my_text <- gsub(my_text, pattern = ">>", replace = "") 
+  my_text <- gsub(my_text, pattern = "<<", replace = "") 
+  my_text <- gsub(my_text, pattern = "__", replace = "") 
+  
+  my_text <- gsub(my_text, pattern = " -", replace = " ") 
+  my_text <- gsub(my_text, pattern = "- ", replace = " ") 
+
+  ## Informative punctuation (brackets, ampersands, etc...)  
+  my_text <- gsub(my_text, pattern = "\\{", replace = " \\{ ")
+  my_text <- gsub(my_text, pattern = "\\}", replace = " \\} ")
+  my_text <- gsub(my_text, pattern = "\\&", replace = " \\& ") 
+  my_text <- gsub(my_text, pattern = "\\$", replace = " $ ") 
+  
+  # Tildes and carats - non-informative punctuation
+  my_text <- gsub(my_text, pattern = "\\^\\^", replace = "") 
+  my_text <- gsub(my_text, pattern = "\\~", replace = " ") 
+  my_text <- gsub(my_text, pattern = "  ", replace = " ")
+  
+  # Make lower case
+  my_text <- tolower(my_text)
+  
+  # Return output
+  return( my_text )  
+}
+
+#' download_wordvecs
+#' 
+#' Downloads default word vector dictionary to 'readme' install directory. 
+#' 
+#' @param url URL of word vector file. Defaults to the pre-trained GloVe Wikipedia 200-dimensional vectors.
+#' @param targetDir Target directory to download files. If NULL uses readme installation directory.
+#' 
+#' @return Path to downloaded word vector dictionary
+#' 
+#' @export 
+#' 
+#' 
+download_wordvecs <- function(url = "http://gking-projects.iq.harvard.edu/files/glove.6B.200d.zip", targetDir=NULL){
+  ## Get the target directory if NULL
+  if (is.null(targetDir)){
+    targetDir = find.package("readme")
+  }
+  
+  ## Get the filename of the url
+  filename = basename(url)
+  
+  ## Does the file exist already
+  if (file.exists(file.path(targetDir, filename))){
+    stop(paste(file.path(targetDir, filename), "already exists"))
+  }else{
+    ## Download the file
+    download.file(url, destfile=file.path(targetDir, filename))
+    cat("Download complete.\n")
+    ## If the file is a zip, unpack it
+    if (tools::file_ext(file.path(targetDir, filename)) == "zip"){
+      ## Unpack the file
+      cat("Unpacking zip archive.\n")
+      ## Unzipping
+      unzippedFiles = unzip(file.path(targetDir, filename), exdir = targetDir)
+      cat("Complete.\n")
+      return(unzippedFiles)
+    }else{
+      return(file.path(targetDir, filename))
+    }
+    
+  }
+  
+}
+
+
+Winsorize_values <- function(x){ 
+  sum_x <- summary(x); qr_ <- 1.5*diff(sum_x[c(2,5)]);
+  return( c( sum_x[2] - qr_,  sum_x[5]+qr_) )  
+}
+
+vec2prob <- function(.){x <- table(.); x/sum(x)}
+
+f2n <- function(.){as.numeric(as.character(.))}
+
+colSds <- function (x, center = NULL, dim. = dim(x)){ n <- dim.[1]; x <- x * x; x <- colMeans(x); x <- (x - center^2); sqrt (  x * (n/(n - 1)) )  }
+FastScale <- function(x,cm=NULL,csd=NULL){
+  if(is.null(cm)){cm = .colMeans(x, m = nrow(x), n = ncol(x))}# Get the column means
+  if(is.null(csd)){csd = colSds(x, center = cm)}# Get the column sd
+  return( t( (t(x) - cm) / csd ) )
+} 
+
+readme_est_fxn <- function(X, Y){
+  solution_ <- try(limSolve::lsei(A = X, B=Y, E=matrix(nrow=1, ncol=ncol(X), data=rep(1., ncol(X))), 
+                                                                          F=c(1.), G=diag(rep(1, ncol(X))), H = rep(0., ncol(X)))$X, TRUE)
+  return( solution_ )  
+  }
---
+++ optimalcausalities/R/readme2_undergrad.R
@@ -0,0 +1,191 @@
+#' undergrad
+#' 
+#' Preprocessing for \code{readme} function - creates a document-feature matrix 
+#' (saved as a data frame in output) to be passed to \code{readme}. Users can either input word-specific vectors 
+#' using the \code{wordVecs_corpus} or \code{wordVecs_corpusPointer} parameters. Primarily intended for users 
+#' wanting control over the pre-processing protocol. 
+#'
+#' @param documentText A vector in which each entry corresponds to a ``clean'' document. 
+#' Note that the function will take as a ``word'' all whitespace-separated elements in each vector entry. For example,
+#' \code{"star."} would have to have an exact analogue in the vector corpus, otherwise
+#' it will be dropped in the calculations.
+#' 
+#' @param wordVecs A matrix where each row denotes a word and each column a word vector. Words should be stored as the rownames of the matrix.
+#' 
+#' @param word_quantiles A numeric vector denoting the quantiles (0-1) used to summarize each word vector dimension. Defaults to 0.10th, 0.50th and 0.90th quantiles.
+#' 
+#' @param reattempt If TRUE, attempts to match terms missing from the wordVec corpus with alternate representations.
+#' 
+#' @param reattempt_regex A list of character vectors containing regular expression pairs to be used for generating alternate representations of words to attempt
+#' to match with the wordVec corpus when terms initially cannot be matched. Order matters. 
+#' 
+#' @param unique_terms If TRUE, removes duplicate terms from each document - each document is represented only by the presence or absence of a term.
+#' 
+#' @param verbose If TRUE, prints updates as function runs
+#' 
+#' @return A data.frame consisting of the \code{word_quantiles} quantiles of the word vectors by document.
+#'  Each row corresonds to a document, and the columns to a particular summary of a particular word vector dimension. 
+#' 
+#' @examples 
+#' #set seed 
+#' set.seed(1) 
+#' 
+#' #Generate synthetic word vector corpus. 
+#' my_wordVecs <- matrix(rnorm(11*50), ncol = 50)
+#' row.names(my_wordVecs) <- c("the","true", "thine", "stars", "are" ,
+#'                               "fire", ".", "to", "own", "self", "be")
+#' 
+#' #Setup ``documents'' 
+#' my_documentText <- c(
+#' "the stars are fire .", #document 1 
+#' "to thine own self be true ", #document 2 
+#' "true stars be true ." #document 3
+#' )
+#' 
+#' #Get document-level word vector summaries. 
+#' my_dfm <- undergrad(documentText = my_documentText, wordVecs = my_wordVecs)
+#' print( my_dfm ) 
+#' 
+#' @export 
+#' 
+undergrad <- function(documentText, wordVecs = NULL, word_quantiles = c(.1, .5, .9), reattempt = T, 
+                      reattempt_regex = list(c("\\#",""), c("#\\S+", "<hashtag>"), 
+                                            c("[[:punct:]]+", ''),c('ing\\b',''),
+                                            c('s\\b', ''),c('ed\\b', ''),c('ies\\b', 'y')), 
+                      unique_terms = T, verbose=T){ 
+    if(is.null(wordVecs)){ 
+      cat("NOTE: No word vector matrix specified in 'wordVecs' - Searching for default wordvecs in install directory\n
+Searching for: 'glove.6B.200d.txt'\n")
+      targetDir = find.package("readme")
+      if (file.exists(file.path(targetDir, "glove.6B.200d.txt"))){
+        wordVecs_corpus <- data.table::fread(file.path(targetDir, "glove.6B.200d.txt"))
+        wordVecs_keys <- wordVecs_corpus[[1]]## first row is the name of the term
+        wordVecs_corpus <- as.matrix (  wordVecs_corpus[,-1] )  #
+        row.names(wordVecs_corpus) <- wordVecs_keys
+        wordVecs <- wordVecs_corpus
+        rm(wordVecs_corpus)
+        rm(wordVecs_keys)## Remove the original loaded table to save space
+        
+      }else{
+        stop("Could not find default word vector summaries. Please run download_wordvecs() to obtain the default word vector dictionary\n
+          or manually provide a data frame containing the word vectors.\n
+             We recommend using a GloVe corpus from https://nlp.stanford.edu/projects/glove/\n")
+        
+      }
+          
+
+    }
+    ## Sanity check the wordVecs
+    if (!is.matrix(wordVecs)){
+      stop("Error: 'wordVecs' is not a matrix")
+    }
+
+    ## Tokenize the documents using whitespace splits
+    tokenized_docs <- tokenizers::tokenize_regex(documentText)
+      
+    ### Consider only unique terms
+    if (unique_terms == T){
+      tokenized_docs <- lapply(tokenized_docs, function(x) return(  unique(x[x!=""])) )
+    }
+    
+    ## Drop any documents that have zero terms
+    num_terms <- sapply(tokenized_docs, function(x) length(x))
+    if (length(which(num_terms == 0)) > 0){
+      cat(paste("WARNING: Document ", which(num_terms == 0), " has no terms, dropping from analysis...\n", collapse = "", sep = ""))
+    }
+
+    ### wordVec terms
+    wordVec_terms <- rownames(wordVecs)
+    
+    ### Number of unique stems
+    unique_stems <- unique(unlist(tokenized_docs))
+    
+    if(verbose == T){
+      cat(paste("Processing...\n"))
+      cat(paste("Number of documents: ", length(documentText), "\n", sep=""))
+      cat(paste("Number of unique word stems: ", length(unique_stems), "\n", sep=""))
+      cat(paste("Number of word vector terms: ", nrow(wordVecs), "\n", sep=""))
+    }
+    
+    ### How many of the unique stems match the word vector matrix
+    unique_stem_match <- match(unique_stems, wordVec_terms)
+    names(unique_stem_match) <- unique_stems
+    ### 
+    if (verbose == T){
+      cat(paste("First attempt: Matched ", sum(!is.na(unique_stem_match)), " of ",  length(unique_stems), " ", "(",
+                round(sum(!is.na(unique_stem_match))/length(unique_stems), 3)*100, "%) ", "terms to word vectors\n", sep=""))
+    }
+    
+    ### If user wants to retry to match some of the missing terms
+    if (reattempt == T){
+      ### Which terms didn't get a match
+      missing_stems <- unique_stems[is.na(unique_stem_match)]
+      
+      ### Missing matches
+      match_missing <- rep(NA, length(missing_stems))
+      
+      ### For each reg-ex pair in reattempt_regex
+      for (indx in 1:length(reattempt_regex)){
+        ## Attempt to match given the substitution
+        match_missing[is.na(match_missing)] <- match(gsub(missing_stems[is.na(match_missing)], 
+                                                          pattern = reattempt_regex[[indx]][1], 
+                                                          replace = reattempt_regex[[indx]][2]), 
+                                                     wordVec_terms)
+        
+      }
+    
+      ## Save the substituted matches
+      unique_stem_match[missing_stems] <- match_missing
+    }
+    
+    if (verbose == T){
+      cat(paste("Second attempt: Matched ", sum(!is.na(unique_stem_match)), " of ",  length(unique_stems), " ", "(",
+                round(sum(!is.na(unique_stem_match))/length(unique_stems), 3)*100, "%) ", "terms to word vectors\n", sep=""))
+    }
+    
+    ### Map each document to the relevant row of the wordVecs matrix
+    matched_terms <- lapply(tokenized_docs, function(x) unique_stem_match[x])
+    
+    ### Check if any documents had zero matches
+    num_terms_matched <- sapply(matched_terms, function(x) sum(!is.na(x)))
+    
+    ### Drop missing word vectors
+    matched_terms_noNA <- lapply(matched_terms, function(x) x[!is.na(x)])
+    
+    ### Document-vector matrices
+    document_matrices <- lapply(matched_terms_noNA, function(x) { 
+                                  if(length(x) > 1){ docMatrix = wordVecs[x,] } 
+                                  if(length(x) == 1){ docMatrix = t(wordVecs[x,]) } 
+                                  if(length(x) == 0){ docMatrix = t(rep(NA, times = ncol(wordVecs))) }
+                                return( docMatrix )  
+                                })
+    
+    if (verbose == T){
+      cat("Computing word vector summaries for each document...\n")
+    }
+
+    ### Calculate the summary
+    dfm <- do.call(rbind, lapply(document_matrices, function(x){ 
+                                    c(apply(x, 2, function(x_col){ 
+                                      quantile(x_col, c(word_quantiles), na.rm = T)
+                                  }))}))
+    dfm = apply(dfm, 2, function(x){
+      x[is.na(x)] <- mean(x,na.rm = T)
+      return( x ) 
+    })
+    colnames(dfm) <- c(sapply(colnames(wordVecs), 
+                              function(z){ paste(z, "_", round(100*word_quantiles), "th_Quantile", sep = "") }))
+     
+    ### Are any columns zero-variance?
+    column_sds <- apply(dfm, 2, sd)
+    
+    ###
+    if (length(which(column_sds == 0)) > 0){
+      
+      if (verbose == T){
+        cat(paste("WARNING: Feature matrix ", colnames(dfm)[which(column_sds == 0)], " has zero variance, dropping from analysis...\n", collapse = "", sep = ""))
+      }
+    }
+    
+    return(dfm[,column_sds != 0])
+}
