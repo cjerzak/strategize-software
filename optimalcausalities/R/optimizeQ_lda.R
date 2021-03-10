563312ba81592f44da81f4bb1586c8ef85edca32
cjerzak <connor.jerzak@gmail.com>
2021-03-10 07:37:39 -0500

    add

---
+++ optimalcausalities/R/optimizeQ_lda.R
@@ -0,0 +1,27 @@
+#' computeQ_lda
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke Imai, Rohit  Working Paper.
+#' \emph{An Improved Method of Automated Nonparametric Content Analysis for Social Science}.
+#' \url{https://gking.harvard.edu/words}
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @import
+#' @export
+function_ <- function()
