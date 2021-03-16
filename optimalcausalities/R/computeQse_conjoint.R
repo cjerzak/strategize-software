39fa631f72c23366d3df3a35b5e05b6c0e58de57
cjerzak <connor.jerzak@gmail.com>
2021-03-16 11:58:11 -0400

    eve

---
+++ optimalcausalities/R/computeQse_conjoint.R
@@ -0,0 +1,29 @@
+#' computeQse_conjoint
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke, Rohit, Connor.  Working Paper.
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @export
+
+
+computeQse_conjoint <- function(theta,FactorMatrix,log_pr_w=NULL){
+
+
+}
---
+++ optimalcausalities/R/wrapper.R
@@ -0,0 +1,28 @@
+#' optimizecausalities
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke Imai, Rohit, Connor
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @export
+
+optimizecausalities <- function(Yobs, W, optimize = T, theta = NULL){
+
+
+}
