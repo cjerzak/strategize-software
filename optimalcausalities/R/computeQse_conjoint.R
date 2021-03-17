1ab59268916835870a61e1cf3c25dea518ddc71f
cjerzak <connor.jerzak@gmail.com>
2021-03-17 10:31:45 -0400

    update

--- optimalcausalities/R/computeQse_conjoint.R
+++ optimalcausalities/R/computeQse_conjoint.R
@@ -30,33 +30,38 @@ computeQse_conjoint <- function(FactorsMat, Yobs,
                                 hajek = T,
                                 returnLog = T,
                                 log_treatment_combs){
-  low_maxProb <- sum(log(
-    sapply(1:ncol(FactorsMat),function(ze){
-      max(assignmentProbList[[ze]]) })
-  ))
-
   if(is.null(log_pr_w)){
     log_pr_w = rowSums(log(
       sapply(1:ncol(FactorsMat),function(ze){
         (assignmentProbList[[ze]][ FactorsMat[,ze] ]) })
     ))#fast match here
   }
-  my_vec <- FactorsMat[,ze]
-  my_vec[] <- 1
-  my_vec <- as.numeric(my_vec)
-  log_pr_new = rowSums(log(
+
+
+  # Perform weighting to obtain bound for E_theta[c_t]
+  {
+    log_pr_new = rowSums(log(
+      sapply(1:ncol(FactorsMat),function(ze){
+      (hypotheticalProbList[[ze]][ FactorsMat[,ze] ] ) })
+    ))
+
+    my_wts = exp(log_pr_new   - log_pr_w  )
+    if(hajek == T){ my_wts <- my_wts / sum(my_wts);scaleFactor = sum(Yobs^2 * my_wts )   }
+    if(hajek == F){ scaleFactor <- mean(Yobs^2 * my_wts )   }
+  }
+
+  # Compute max prob (take maximum prob. of each Multinomial)
+  log_maxProb <- sum(log(
     sapply(1:ncol(FactorsMat),function(ze){
-    (hypotheticalProbList[[ze]][ FactorsMat[,ze] ] ) })
+      max(hypotheticalProbList[[ze]]) })
   ))
 
-  my_wts = exp(log_pr_new   - log_pr_w  )
-  if(hajek == T){ my_wts <- my_wts / sum(my_wts);scaleFactor = sum(Yobs^2 * my_wts )   }
-  if(hajek == F){ scaleFactor <- mean(Yobs^2 * my_wts )   }
-
-  upperBound_se_VE_log = (log(scaleFactor) + log_treatment_combs + low_maxProb - log(length(Yobs)))
-  upperBound_se_EV_log = (log(var(Yobs)) + log_treatment_combs + low_maxProb - log(length(Yobs)))
+  # Combine terms to get VE and EV
+  upperBound_se_VE_log = (log(scaleFactor) + log_treatment_combs + log_maxProb - log(length(Yobs)))
+  upperBound_se_EV_log = (log(var(Yobs)) + log_treatment_combs + log_maxProb - log(length(Yobs)))
   upperBound_se_ <- 0.5*matrixStats::logSumExp(c(upperBound_se_EV_log,upperBound_se_VE_log))#0.5 for sqrt
 
+  # log scale is used in optimization to improve numerical stability
   if(returnLog == F){upperBound_se_ <- exp(upperBound_se_) }
   return( upperBound_se_ )
 }
--- optimalcausalities/R/computeQse_lda.R
+++ optimalcausalities/R/computeQse_lda.R
@@ -27,6 +27,8 @@ computeQse_lda = function(THETA__,INDICES_, DOC_INDICES_U, D_INDICES_U,
                             MODAL_DOC_LEN,
                             TERMS_MAT_INPUT,LOG_TREATCOMBS,YOBS,
                             returnLog = T,LOG_PR_W=NULL){
+
+    # Set up greedy algorithm for computing log_maxProb
     logW = log(apply( term_mat * c(THETA__),2,sum))
     logW = logW[names(MARGINAL_BOUNDS)];
     logW_counter = logW; logW_counter[] <- 0
@@ -35,8 +37,9 @@ computeQse_lda = function(THETA__,INDICES_, DOC_INDICES_U, D_INDICES_U,
       my_tmp[fa] <- names(which_max)
       log_maxProb <- log_maxProb + logW[names(which_max)]
       logW_counter[names(which_max)] <- logW_counter[names(which_max)] + 1
-    }
-    #table(my_tmp); MARGINAL_BOUNDS[names(table(my_tmp))]
+    }  #check that marginal bounds respected: table(my_tmp); MARGINAL_BOUNDS[names(table(my_tmp))]
+
+    # Perform weighting to obtain scaling factor
     THETA_MAT_ = PI_MAT_INPUT; THETA_MAT_[] <- THETA__
     NUM__ = PrWGivenPi_fxn(doc_indices     = DOC_LIST[INDICES_],
                            pi_mat = THETA_MAT_[,INDICES_],
@@ -51,8 +54,9 @@ computeQse_lda = function(THETA__,INDICES_, DOC_INDICES_U, D_INDICES_U,
                              doc_indices_u = DOC_INDICES_U,
                              d_indices_u = D_INDICES_U, log_=T)
     }
-    MY_WTS__ = prop.table(exp(NUM__ - LOG_PR_W  ))
-    scaleFactor = sum(YOBS[INDICES_]^2 * MY_WTS__)
+    scaleFactor = sum(YOBS[INDICES_]^2 * prop.table(exp(NUM__ - LOG_PR_W  ))  )
+
+    # Combine terms to get overall variance - log scale for improved numerical stability
     upperBound_se_VE_log = (log(scaleFactor) + LOG_TREATCOMBS + log_maxProb - log(length(INDICES_)))
     upperBound_se_EV_log = (log(var(YOBS[INDICES_])) + LOG_TREATCOMBS + log_maxProb - log(length(INDICES_)))
     upperBound_se_ <- 0.5*matrixStats::logSumExp(c(upperBound_se_EV_log,upperBound_se_VE_log))#0.5 for sqrt
