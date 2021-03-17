5a11e7951668e151118c063ab5bbf6bed7bde982
cjerzak <connor.jerzak@gmail.com>
2021-03-17 07:22:57 -0400

    tmp

--- documentPackage.R
+++ documentPackage.R
@@ -3,6 +3,7 @@ setwd("~/Documents/optimalcausalities-software")
 package_path <- "~/Documents/optimalcausalities-software/optimalcausalities"
 
 devtools::document(package_path)
+try(file.remove(sprintf("./optimalcausalities.pdf")),T)
 system(paste(shQuote(file.path(R.home("bin"), "R")),
              "CMD", "Rd2pdf", shQuote(package_path)))
 
