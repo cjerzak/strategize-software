d8184b7ef2e4a765992a8532b554a94d83b96766
cjerzak <connor.jerzak@gmail.com>
2021-03-23 19:52:22 -0400

    adds

---
+++ optimalcausalities/R/wrappers.R
@@ -0,0 +1,150 @@
+#' specify
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke Imai, Rohit, Connor
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @export
+
+specify_treatmentMechanism <- function(Yobs,
+                    W,
+                    PrW_parameters = list()){
+    if(names(PrW_parameters) %in% "pi_mat"){
+
+    }
+
+  if(!names(PrW_parameters) %in% "pi_mat"){
+
+  }
+}
+
+
+
+
+#' find_optimalStrategy
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke Imai, Rohit, Connor
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @export
+
+find_optimalStrategy <- function(specifiedAssignmentMechanism=NULL,
+                                 cubeConstraint = F){
+  if(names(PrW_parameters) %in% "pi_mat"){
+
+  }
+
+  if(!names(PrW_parameters) %in% "pi_mat"){
+
+  }
+}
+
+
+
+#' analyze_fixedStrategy
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke Imai, Rohit, Connor
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @export
+
+analyze_fixedStrategy <- function(specifiedAssignmentMechanism=NULL,
+                             hypotheticalAssignmentMechanism=NULL){
+  if(names(PrW_parameters) %in% "pi_mat"){
+
+  }
+
+  if(!names(PrW_parameters) %in% "pi_mat"){
+
+  }
+}
+
+
+
+#' plot_optimalStrategy
+#'
+#' Implements ...
+#'
+#' @param dfm 'document-feature matrix'. A list ...
+
+#' @return A list consiting of \itemize{
+#'   \item Items.
+#' }
+#'
+#' @section References:
+#' \itemize{
+#' \item Kosuke Imai, Rohit, Connor
+#' }
+#'
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- rnorm(100)
+#'
+#' @export
+
+plot_optimalStrategy <- function(specifiedAssignmentMechanism=NULL,
+                                  hypotheticalAssignmentMechanism=NULL){
+  if(names(PrW_parameters) %in% "pi_mat"){
+
+  }
+
+  if(!names(PrW_parameters) %in% "pi_mat"){
+
+  }
+}
+
+
+
