211eb1ce12f03c975d7ac827a2bc8b0becb54278
cjerzak <connor.jerzak@gmail.com>
2021-03-29 15:39:22 -0400

    tmp

--- optimalcausalities/R/computeQ_conjoint_internal.R
+++ optimalcausalities/R/computeQ_conjoint_internal.R
@@ -6,30 +6,30 @@ computeQ_conjoint_internal <- function(FactorsMat_internal, Yobs_internal,
                               computeLB  = F){
     if(is.null(log_pr_w_internal)){
       log_pr_w_internal = rowSums(log(
-        sapply(1:ncol(FactorsMat),function(ze){
-        (assignmentProbList[[ze]][ FactorsMat[,ze] ]) })
+        sapply(1:ncol(FactorsMat_internal),function(ze){
+        (assignmentProbList_internal[[ze]][ FactorsMat_internal[,ze] ]) })
         ))
     }
     log_pr_w_new <- rowSums(log(
-      sapply(1:ncol(FactorsMat),function(ze){
-        hypotheticalProbList[[ze]][ FactorsMat[,ze] ]  })
+      sapply(1:ncol(FactorsMat_internal),function(ze){
+        hypotheticalProbList_internal[[ze]][ FactorsMat_internal[,ze] ]  })
     ))
-    my_wts = exp(  log_pr_w_new   - log_pr_w  )
+    my_wts = exp(  log_pr_w_new   - log_pr_w_internal  )
     if(hajek == T){
       my_wts <- my_wts / sum(my_wts);
-      if(computeLB == F){ Qest = sum(Yobs * my_wts )  }
+      if(computeLB == F){ Qest = sum(Yobs_internal * my_wts )  }
       if(computeLB == T){
-        minValue <- min(Yobs)
-        Yobs_nonZero <- Yobs + (abs(minValue) + 1)*(minValue <= 0)
-        #Qest <- exp(1/length(Yobs)*sum(log(Yobs_nonZero)+log(my_wts)))
+        minValue <- min(Yobs_internal)
+        Yobs_nonZero <- Yobs_internal + (abs(minValue) + 1)*(minValue <= 0)
+        #Qest <- exp(1/length(Yobs_internal)*sum(log(Yobs_nonZero)+log(my_wts)))
         Qest <- sum(log(Yobs_nonZero)+log(my_wts))
       }
     }
     if(hajek == F){
-      if(computeLB == F){ Qest <- mean(Yobs * my_wts )   }
+      if(computeLB == F){ Qest <- mean(Yobs_internal * my_wts )   }
       if(computeLB == T){
-        minValue <- min(Yobs)
-        Yobs_nonZero <- Yobs + (abs(minValue) + 1)*(minValue <= 0)
+        minValue <- min(Yobs_internal)
+        Yobs_nonZero <- Yobs_internal + (abs(minValue) + 1)*(minValue <= 0)
         Qest <- mean(log(Yobs_nonZero)+log(my_wts))
       }
     }
