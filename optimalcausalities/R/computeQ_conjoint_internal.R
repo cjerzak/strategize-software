a9064e38228904be3fb61ec182545efb01a5925a
cjerzak <connor.jerzak@gmail.com>
2021-04-23 14:44:59 -0400

    tmp

--- optimalcausalities/R/computeQ_conjoint_internal.R
+++ optimalcausalities/R/computeQ_conjoint_internal.R
@@ -1,22 +1,28 @@
-computeQ_conjoint_internal <- function(FactorsMat_internal, Yobs_internal,
+computeQ_conjoint_internal <- function(FactorsMat_internal,
+                              Yobs_internal,
+                              FactorsMat_internal_mapped = NULL,
                               hypotheticalProbList_internal,
                               assignmentProbList_internal,
                               log_pr_w_internal = NULL,
-                              hajek = T,
+                              hajek = T, knownNormalizationFactor = NULL,
                               computeLB  = F){
     if(is.null(log_pr_w_internal)){
-      log_pr_w_internal = rowSums(log(
-        sapply(1:ncol(FactorsMat_internal),function(ze){
-        (assignmentProbList_internal[[ze]][ FactorsMat_internal[,ze] ]) })
-        ))
+      log_pr_w_internal <- log( sapply(1:ncol(FactorsMat_internal),function(ze){
+          (assignmentProbList_internal[[ze]][ FactorsMat_internal[,ze] ]) }) )
+      if(all(class(log_pr_w_internal) == "numeric")){ log_pr_w_internal <- sum(log_pr_w_internal)}
+      if(any(class(log_pr_w_internal) != "numeric")){ log_pr_w_internal = rowsums(log_pr_w_internal)}
     }
-    log_pr_w_new <- rowSums(log(
-      sapply(1:ncol(FactorsMat_internal),function(ze){
-        hypotheticalProbList_internal[[ze]][ FactorsMat_internal[,ze] ]  })
-    ))
-    my_wts = exp(  log_pr_w_new   - log_pr_w_internal  )
+
+    # new probability
+    if(!is.null(FactorsMat_internal_mapped)){FactorsMat_internal_mapped[] <- log(unlist(hypotheticalProbList_internal)[FactorsMat_internal_mapped])}
+    if(is.null(FactorsMat_internal_mapped)){FactorsMat_internal_mapped <- log( sapply(1:ncol(FactorsMat_internal),function(ze){hypotheticalProbList_internal[[ze]][ FactorsMat_internal[,ze] ]  })  )}
+    if(any(class(FactorsMat_internal_mapped) != "numeric")){ log_pr_w_new <- rowsums(FactorsMat_internal_mapped)}
+    if(all(class(FactorsMat_internal_mapped) == "numeric")){ log_pr_w_new <- sum(FactorsMat_internal_mapped)}
+    my_wts <- exp(  log_pr_w_new   - log_pr_w_internal  )
+    sum_raw_wts <- sum( my_wts )
     if(hajek == T){
-      my_wts <- my_wts / sum(my_wts);
+      if(is.null(knownNormalizationFactor)){  my_wts <- my_wts / sum_raw_wts }
+      if(!is.null(knownNormalizationFactor)){  my_wts <- my_wts / knownNormalizationFactor }
       if(computeLB == F){ Qest = sum(Yobs_internal * my_wts )  }
       if(computeLB == T){
         minValue <- min(Yobs_internal)
@@ -36,6 +42,7 @@ computeQ_conjoint_internal <- function(FactorsMat_internal, Yobs_internal,
 
     return(list("Qest"=Qest,
                 "Q_wts"=my_wts,
+                "Q_wts_raw_sum" = sum_raw_wts,
                 "log_pr_w_new"=log_pr_w_new,
                 "log_pr_w"=log_pr_w_internal))
 }
--- optimalcausalities/R/computeQse_conjoint.R
+++ optimalcausalities/R/computeQse_conjoint.R
@@ -1,8 +1,11 @@
 computeQse_conjoint <- function(FactorsMat, Yobs,
                                 hypotheticalProbList,
                                 assignmentProbList,
+                                FactorsMat_internal_mapped = NULL,
                                 log_pr_w = NULL,
                                 hajek = T,
+                                knownNormalizationFactor = NULL,
+                                knownSigma2 = NULL,
                                 hypotheticalN = NULL,
                                 returnLog = T,
                                 log_treatment_combs=NULL){
@@ -14,22 +17,28 @@ computeQse_conjoint <- function(FactorsMat, Yobs,
   }
 
   if(is.null(log_pr_w)){
-    log_pr_w = rowSums(log(
+    log_pr_w <- log(
       sapply(1:ncol(FactorsMat),function(ze){
         (assignmentProbList[[ze]][ FactorsMat[,ze] ]) })
-    ))#fast match here
+    )
+    if(all(class(log_pr_w) == "numeric")){ log_pr_w <- sum(log_pr_w)}
+    if(any(class(log_pr_w) != "numeric")){ log_pr_w <- rowsums(log_pr_w)}
   }
 
 
   # Perform weighting to obtain bound for E_theta[c_t]
   {
-    log_pr_new = rowSums(log(
-      sapply(1:ncol(FactorsMat),function(ze){
-      (hypotheticalProbList[[ze]][ FactorsMat[,ze] ] ) })
-    ))
+    if(!is.null(FactorsMat_internal_mapped)){FactorsMat_internal_mapped[] <- log(unlist(hypotheticalProbList)[FactorsMat_internal_mapped])}
+    if(is.null(FactorsMat_internal_mapped)){FactorsMat_internal_mapped <- log( sapply(1:ncol(FactorsMat),function(ze){hypotheticalProbList[[ze]][ FactorsMat[,ze] ]  })  )}
+    if(any(class(FactorsMat_internal_mapped) != "numeric")){ log_pr_w_new <- rowsums(FactorsMat_internal_mapped)}
+    if(all(class(FactorsMat_internal_mapped) == "numeric")){ log_pr_w_new <- sum(FactorsMat_internal_mapped)}
 
-    my_wts = exp(log_pr_new   - log_pr_w  )
-    if(hajek == T){ my_wts <- my_wts / sum(my_wts);scaleFactor = sum(Yobs^2 * my_wts )   }
+    my_wts = exp(log_pr_w_new   - log_pr_w  )
+    if(hajek == T){
+      if(is.null(knownNormalizationFactor)){  my_wts <- my_wts / sum(my_wts)}
+      if(!is.null(knownNormalizationFactor)){  my_wts <- my_wts / knownNormalizationFactor}
+      scaleFactor = sum(Yobs^2 * my_wts )
+    }
     if(hajek == F){ scaleFactor <- mean(Yobs^2 * my_wts )   }
   }
 
@@ -39,11 +48,14 @@ computeQse_conjoint <- function(FactorsMat, Yobs,
       max(hypotheticalProbList[[ze]]) })
   ))
 
+  if(!is.null(knownSigma2)){ sigma2_hat <- knownSigma2 }
+  if(is.null(knownSigma2)){ sigma2_hat <- var(Yobs) }
+
   # Combine terms to get VE and EV
   logN <- log(length(Yobs))
   if(!is.null(hypotheticalN)){logN <- log(hypotheticalN)}
   upperBound_se_VE_log = (log(scaleFactor) + log_treatment_combs + log_maxProb - logN)
-  upperBound_se_EV_log = (log(var(Yobs)) + log_treatment_combs + log_maxProb - logN)
+  upperBound_se_EV_log = (log(sigma2_hat) + log_treatment_combs + log_maxProb - logN)
   upperBound_se_ <- 0.5*matrixStats::logSumExp(c(upperBound_se_EV_log,upperBound_se_VE_log))#0.5 for sqrt
 
   # log scale is used in optimization to improve numerical stability
