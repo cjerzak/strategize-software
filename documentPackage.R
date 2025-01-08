de49fe57ba4ace24740fa0892b3adef4501c0d16
Connor Jerzak <cjerzak@dhcp-10-250-43-148.harvard.edu>
2025-01-08 10:17:02 -0500

    lic

--- documentPackage.R
+++ documentPackage.R
@@ -1,17 +1,19 @@
-setwd("~/Documents/strategize-software")
-
-package_path <- "~/Documents/strategize-software/strategize"
-
-devtools::document(package_path)
-try(file.remove(sprintf("./strategize.pdf")),T)
-system(paste(shQuote(file.path(R.home("bin"), "R")),
-             "CMD", "Rd2pdf", shQuote(package_path)))
-
-# Check package to ensure it meets CRAN standards.
-devtools::check( package_path )
-
-#install.packages(package_path)
-
-#install.packages( "~/Documents/optimalcausalities-software/optimalcausalities",repos = NULL, type = "source")
-
-#install.packages("~/Library/gurobi911/mac64/R/gurobi_9.1-1_R_4.0.2.tgz",repos=NULL)
+{
+  setwd("~/Documents/strategize-software")
+  
+  package_path <- "~/Documents/strategize-software/strategize"
+  
+  devtools::document(package_path)
+  try(file.remove(sprintf("./strategize.pdf")),T)
+  system(paste(shQuote(file.path(R.home("bin"), "R")),
+               "CMD", "Rd2pdf", shQuote(package_path)))
+  
+  # Check package to ensure it meets CRAN standards.
+  devtools::check( package_path )
+  
+  #install.packages(package_path)
+  
+  #install.packages( "~/Documents/optimalcausalities-software/optimalcausalities",repos = NULL, type = "source")
+  
+  #install.packages("~/Library/gurobi911/mac64/R/gurobi_9.1-1_R_4.0.2.tgz",repos=NULL)
+}
