a3821ec3a619cb28ed47f62402843573aff011a7
cjerzak <connor.jerzak@gmail.com>
2025-09-14 13:02:39 -0400

    remove oryx

--- strategize/R/CS_InitializeJAX.R
+++ strategize/R/CS_InitializeJAX.R
@@ -2,7 +2,6 @@ initialize_jax <- function(conda_env = "strategize_env",
                            conda_env_required = TRUE) {
   library(reticulate)
   # Load reticulate (Declared in Imports: in DESCRIPTION)
-  browser()
   reticulate::use_condaenv(condaenv = conda_env, required = conda_env_required)
   
   # Import Python packages once, storing them in strenv
@@ -13,12 +12,10 @@ initialize_jax <- function(conda_env = "strategize_env",
   strenv$py_gc  <- reticulate::import("gc")
   strenv$optax  <- reticulate::import("optax")
   
-  # Disable 64-bit computations
-  strenv$jax$config$update("jax_enable_x64", FALSE)
-  strenv$jaxFloatType <- strenv$jnp$float32
   
-  # setup numerical precision for delta method
-  strenv$dtj <- strenv$jnp$float64; strenv$jax$config$update("jax_enable_x64", T) # use float64
+  # setup numerical precisions
+  strenv$jaxFloatType <- strenv$jnp$float32
+  strenv$dtj <- strenv$jnp$float64; strenv$jax$config$update("jax_enable_x64", TRUE) # use float64
   #strenv$dtj <- strenv$jnp$float32; strenv$jax$config$update("jax_enable_x64", F) # use float32
 }
 strenv <- new.env( parent = emptyenv() )
